5bf66a481cc2fd40e754428ba26466c3a387f1c5
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2024-03-29 12:33:21 -0700

    chore: solc as peer dependency

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,8 +6,8 @@ require(`@skybit/hardhat-yul`)
 BigInt.prototype[`toJSON`] = () => this.toString() // To prevent TypeError: Do not know how to serialize a BigInt
 
 // SET YOUR ACCOUNT HERE
-const accounts = { mnemonic: process.env.MNEMONIC || `test test test test test test test test test test test junk` }
-// const accounts = [process.env.PRIVATE_KEY0]
+// const accounts = { mnemonic: process.env.MNEMONIC || `test test test test test test test test test test test junk` }
+const accounts = [process.env.PRIVATE_KEY0]
 // const accounts = [process.env.PRIVATE_KEY1, process.env.PRIVATE_KEY2]
 
 // You can add more blockchains to this list if they don't already exist in @wagmi/chains
